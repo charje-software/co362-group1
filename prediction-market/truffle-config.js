ba4cd07f07ea876251398faf663c32f944dac178
Hannah Schlueter <hms4017@ic.ac.uk>
2019-10-11 08:33:25 +0100

    #2 Implement initial CI/CD stages

--- prediction-market/truffle-config.js
+++ prediction-market/truffle-config.js
@@ -9,6 +9,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    gitlab: {
+      host: "trufflesuite-ganache-cli",
+      port: 8545,
+      network_id: "*"
     }
-  }  
+  }
 };
