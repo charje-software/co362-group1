bf47f61cfa3319676a7cca926719770bbdbf71fb
Hannah Schlueter <hms4017@ic.ac.uk>
2019-10-12 21:10:54 +0100

    #2 Reuse the same instance of ganache-cli for each deployment
    Ganache-cli is running in the background on the VM. Each time we migrate the contracts during the deploy stage on master, they are migrated to the same ganache.

--- prediction-market/truffle-config.js
+++ prediction-market/truffle-config.js
@@ -5,12 +5,20 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
+    // use this when testing with local ganache
     development: {
-      host: "127.0.0.1",
+      host: "127.0.0.1",  // localhost
       port: 7545,
       network_id: "*"
     },
-    gitlab: {
+    // used by CI/CD pipeline on master branch when deploying
+    master: {
+      host: "172.17.0.2", // ip of docker container
+      port: 8545,
+      network_id: "*"
+    },
+    // used by CI/CD pipeline during testing stage
+    testing: {
       host: "trufflesuite-ganache-cli",
       port: 8545,
       network_id: "*"
