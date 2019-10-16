d5e92969bbed86b7655fe11a17b857522ec5b542
estherwong777 <estherwong19@gmail.com>
2019-10-16 23:29:48 +0100

    [JEC] #4 Implement placeBet and function to withdraw bet money

--- prediction-market/contracts/PredictionMarket.sol
+++ prediction-market/contracts/PredictionMarket.sol
@@ -2,37 +2,38 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract PredictionMarket {
   address payable[] public agents;
-  address owner;
-  uint256 public balance;
+  address payable owner;
+
   struct Agent {
-    uint256 amountBet;
+    uint amountBet;
   }
 
   mapping(address => Agent) public agentInfo;
 
+  event Distribute(address receiver, uint amount);
+
   constructor() public {
     owner = msg.sender;
-    balance = 0;
   }
 
-  function agentExists(address payable agent) public view returns(bool) {
-    for (uint256 i = 0; i < agents.length; i++) {
+  function agentExists(address agent) public view returns(bool) {
+    for (uint i = 0; i < agents.length; i++) {
       if (agents[i] == agent) return true;
     }
     return false;
   }
 
-  function placeBet(uint256 amount) public payable {
-    require(!agentExists(msg.sender), "Agent has already betted");
-    require(amount == msg.value, "Transferred incorrect amount of Ether");
+  function placeBet() public payable {
+    require(!agentExists(msg.sender), "Agent has already bet");
     agentInfo[msg.sender].amountBet = msg.value;
     agents.push(msg.sender);
-    balance = address(this).balance;
   }
 
-  function distributeWinnings() public {
-    require(agents.length > 0, "No one has betted");
-    address payable winner = agents[0];
-    winner.transfer(agentInfo[winner].amountBet);
+  function distributeWinnings() public payable {
+    require(agentExists(msg.sender), "Agent has not bet");
+    uint value = agentInfo[msg.sender].amountBet;
+    msg.sender.transfer(value);
+    delete agentInfo[msg.sender];
+    agents.length = 0;
   }
 }
