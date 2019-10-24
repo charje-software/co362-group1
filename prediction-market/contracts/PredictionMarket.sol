d7f6f816028532ece873f38282f22fc8980518cc
Hannah <hannah.schlueter17@imperial.ac.uk>
2019-10-24 20:08:39 +0100

    [JEC] #14 Implement 3 continuous stages - betting, ranking, claiming

--- prediction-market/contracts/PredictionMarket.sol
+++ prediction-market/contracts/PredictionMarket.sol
@@ -1,46 +1,130 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract PredictionMarket {
-  address payable[] public agents;
-  address payable owner;
-  uint public totalBets;
+  // Stages enum
+  uint BETTING = 1;
+  uint RANKING = 2;
+  uint CLAIMING = 3;
+
+  // Groups
+  mapping(address => Agent) public group1;
+  mapping(address => Agent) public group2;
+  mapping(address => Agent) public group3;
+  mapping(uint => uint) stageToGroupNumber; // only used in stageToGroup()
+
+  function stageToGroup(uint stage) private view returns(mapping(address => Agent) storage) {
+    uint group = stageToGroupNumber[stage];
+    if (group == 1) {
+      return group1;
+    } else if (group == 2) {
+      return group2;
+    } else {
+      return group3;
+    }
+  }
+
+  mapping(uint => GroupInfo) stageToGroupInfo;
 
   struct Agent {
     uint amountBet;
+    uint demandPrediction;
+    bool won;
   }
 
-  mapping(address => Agent) public agentInfo;
+  // TODO change name
+  struct GroupInfo {
+    address[] agents; // keyset to group
+    uint totalWinners;
+    uint totalBetAmount;
+    uint consumption;
+  }
 
-  event Distribute(address receiver, uint amount);
+  uint WINNING_THRESHOLD = 100;
 
   constructor() public {
-    owner = msg.sender;
-    totalBets = address(this).balance;
+    stageToGroupNumber[BETTING] = 1;
+    stageToGroupNumber[RANKING] = 2;
+    stageToGroupNumber[CLAIMING] = 3;
+
+    // stageToGroupInfo[BETTING] = GroupInfo(agents1, 0, 0, 0);
+    // stageToGroupInfo[RANKING] = GroupInfo(agents2, 0, 0, 0);
+    // stageToGroupInfo[CLAIMING] = GroupInfo(agents3, 0, 0, 0);
   }
 
-  function agentExists(address agent) public view returns(bool) {
-    for (uint i = 0; i < agents.length; i++) {
-      if (agents[i] == agent) return true;
-    }
-    return false;
+
+  // True if agent has placed bet in current betting stage.
+  function hasPlacedBet(address agent) public view returns(bool) {
+    mapping(address => Agent) storage group = stageToGroup(BETTING);
+    return group[agent].amountBet != 0;
+  }
+
+  function canRank(address agent) public view returns(bool) {
+    mapping(address => Agent) storage group = stageToGroup(RANKING);
+    return group[agent].amountBet != 0;
+  }
+
+  function canClaim(address agent) public view returns(bool) {
+    mapping(address => Agent) storage group = stageToGroup(CLAIMING);
+    return group[agent].amountBet != 0;
   }
 
   // Records bet from a specific address. Only allows for one bet per user
-  // until the next day (or whenever distributeWinnings is called) 
-  function placeBet() public payable {
-    require(!agentExists(msg.sender), "Agent has already bet");
-    agentInfo[msg.sender].amountBet = msg.value;
-    agents.push(msg.sender);
-    totalBets = address(this).balance;
+  // until the next day or whenever distributeWinnings is called.
+  function placeBet(uint demandPrediction) public payable {
+    require(!hasPlacedBet(msg.sender), "Agent has already bet");
+
+    mapping(address => Agent) storage group = stageToGroup(BETTING);
+    GroupInfo storage groupInfo = stageToGroupInfo[BETTING];
+
+    group[msg.sender].amountBet = msg.value;
+    group[msg.sender].demandPrediction = demandPrediction;
+    group[msg.sender].won = false;
+    groupInfo.agents.push(msg.sender);
+    groupInfo.totalBetAmount += msg.value;
   }
 
-  // Winnings are transferred back to agent. Currently 
-  // transfers the same amount as you bet 
-  function distributeWinnings() public payable {
-    require(agentExists(msg.sender), "Agent has not bet");
-    uint value = agentInfo[msg.sender].amountBet;
-    msg.sender.transfer(value);
-    delete agentInfo[msg.sender];
+  function rank() public payable {
+    require(canRank(msg.sender), "Agent cannot rank now");
+
+    mapping(address => Agent) storage group = stageToGroup(RANKING);
+    GroupInfo storage groupInfo = stageToGroupInfo[RANKING];
+    uint demandPrediction = group[msg.sender].demandPrediction;
+    if (demandPrediction <= groupInfo.consumption + WINNING_THRESHOLD && demandPrediction >= groupInfo.consumption - WINNING_THRESHOLD) {
+      group[msg.sender].won = true;
+      groupInfo.totalWinners++;
+    }
+  }
+
+  // Distribute winnings back to agents who bet.
+  // TODO agent removing itself from agent key set
+  function claimWinnings() public payable {
+    require(canClaim(msg.sender), "Agent cannot claim at this time");
+
+    mapping(address => Agent) storage group = stageToGroup(CLAIMING);
+    GroupInfo storage groupInfo = stageToGroupInfo[CLAIMING];
+    if (group[msg.sender].won) {
+      uint reward = groupInfo.totalBetAmount / groupInfo.totalWinners;
+      msg.sender.transfer(reward);
+    }
+    delete group[msg.sender];
+  }
+
+  function updateConsumption(uint consumption) public payable {
+    // TODO require that the address of the sender is Oracle
+    address[] storage agents = stageToGroupInfo[CLAIMING].agents;
+    mapping(address => Agent) storage group = stageToGroup(CLAIMING);
+
+    for (uint i = 0; i < agents.length; i++) {
+      delete group[agents[i]];
+    }
+
     agents.length = 0;
+
+    uint tmp = BETTING;
+    BETTING = CLAIMING;
+    CLAIMING = RANKING;
+    RANKING = tmp;
+
+    stageToGroupInfo[RANKING].consumption = consumption;
   }
 }
--- prediction-market/test/PredictionMarketTest.js
+++ prediction-market/test/PredictionMarketTest.js
@@ -1,10 +1,10 @@
 const PredictionMarket = artifacts.require("PredictionMarket");
 
-contract("test: placeBet", async accounts => {
+contract("test: predictionMarket", async accounts => {
   it ('updates balance correctly', async () => {
     let instance = await PredictionMarket.deployed();
-    await instance.placeBet({from: accounts[0], value: 2});
-    await instance.placeBet({from: accounts[1], value: 3});
+    await instance.placeBet(700, {from: accounts[0], value: 2});
+    await instance.placeBet(800, {from: accounts[1], value: 3});
     const totalBets = await instance.totalBets.call();
     assert.equal(totalBets.toString(), '5');
   });
@@ -15,5 +15,20 @@ contract("test: placeBet", async accounts => {
     const agent2 = await instance.agents.call(1);
     assert.equal(agent1, accounts[0]);
     assert.equal(agent2, accounts[1]);
+  });
+
+  it ('records the demand prediction correctly', async () => {
+    let instance = await PredictionMarket.deployed();
+    const agent1 = await instance.agentInfo.call(accounts[0]);
+    const agent2 = await instance.agentInfo.call(accounts[1]);
+    assert.equal(agent1[1], 700);
+    assert.equal(agent2[1], 800);
+  });
+
+  it ('clears bet list after winnings distribution', async () => {
+    let instance = await PredictionMarket.deployed();
+    await instance.distributeWinnings({from: accounts[0]});
+    const totalBets = await instance.totalBets.call();
+    assert.equal(totalBets.toString(), '0');
   });
 });
