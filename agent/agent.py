70d9f7197108b8e55ff71993673655fbbc84dde6
Hannah Schlueter <hannah.schlueter17@imperial.ac.uk>
2019-10-30 09:57:15 +0000

    #26 Add a run method for the agent.
    It allows the agent to participate in a given number of consecutive betting rounds.

--- agent/agent.py
+++ agent/agent.py
@@ -1,3 +1,5 @@
+import time
+
 from prediction_market_adapter import PredictionMarketAdapter
 
 
@@ -18,6 +20,52 @@ class Agent:
     def __init__(self, account=ACCOUNT_1):
         self.account = account
         self.prediction_market = PredictionMarketAdapter()
+
+    def run(self, period_length, rounds=1, logging=True):
+        """
+        Participate in a given number of consecutive betting rounds.
+
+        Args:
+            period_length: Length of betting period. Note that the function will
+                           take (rounds + 3) * period_length seconds to run.
+                           (It also waits after claiming the final reward.)
+            rounds: Number of betting rounds. Default: 1
+            logging: If true, the agent prints each action. Default: True
+        """
+        time.sleep(period_length / 10.0)  # off-set so agent doesn't clash with oracle
+        has_bet = False
+        waiting = False
+        has_ranked = False
+
+        def log(msg):
+            if logging:
+                print(self.account[:8] + ": " + msg)
+
+        while rounds > 0 or has_bet or waiting or has_ranked:
+            if has_ranked:
+                log("Collecting reward.")
+                self.collect_reward()
+                has_ranked = False
+
+            if waiting:
+                log("Ranking prediction.")
+                self.rank_bet()
+                waiting = False
+                has_ranked = True
+
+            if has_bet:
+                log("Waiting.")
+                has_bet = False
+                waiting = True
+
+            if rounds > 0:
+                log("Placing a bet.")
+                self.place_bet()
+                rounds -= 1
+                has_bet = True
+
+            log("Sleeping.")
+            time.sleep(period_length)
 
     def place_bet(self):
         self.prediction_market.place_bet(self.account, Agent.DEFAULT_BETTING_AMOUNT,
